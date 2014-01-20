32b30653dbdc56d693212639f367a742c024962e
raykanani <ray@smartpics.co>
2014-01-20 12:11:35 -0800

    Refactor
    Refactor application.js as one function that targets div that is being
    hovered on and replaces content based on data attribute

--- application.js
+++ application.js
@@ -1,34 +1,25 @@
-var orginalHeadline = $('<h1 class="headline">Making collaboration productive and enjoyable for people every day</h1>');
-var originalSubline = $('<h4 class="subline">Frustration-free web-based apps for collaboration, sharing information, and making decisions.</h4>');
-
 $(function(){
-	$('.basecamp').mouseenter(function(){
-		//Basecamp product content enter and replace
-		$('.headline').replaceWith('<h2 class="headline"><span>Basecamp</span> is the project management tool you wish you had on your last project.</h2>');
-		$('.subline').replaceWith('<h5 class="subline-replace">Are you still managing projects with email? Are you still using Excel for your to-do lists? It’s time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.</h5>');
-		$('.basecamp .arrow-right').css('display', 'inline');
-	});
-	$('.highrise').mouseenter(function(){
-		//Highrise product content enter and replace
-		$('.headline').replaceWith('<h2 class="headline"><span>Highrise</span> remembers the important things about people you’d normally forget.</h2>');
-		$('.subline').replaceWith('<h5 class="subline-replace">Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.</h5>');
-		$('.highrise .arrow-right').css('display', 'inline');
-		//Compress headline
-		$('.headline').css('width', '780px')
-	});
-	$('.campfire').mouseenter(function(){
-		//Campfire product content enter and replace
-		$('.headline').replaceWith('<h2 class="headline">From near or far, <span>Campfire</span> helps teams work together over the web in real-time.</h2>');
-		$('.subline').replaceWith('<h5 class="subline-replace">Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It’s game changing. We couldn’t run our own business without Campfire.</h5>');
-		$('.campfire .arrow-left').css('display', 'inline');
-	});
-	$('.basecamp, .highrise, .campfire').mouseleave(function(){
-		// Revert back original content
-		$('.headline').replaceWith(orginalHeadline);
-		$('.subline-replace').replaceWith(originalSubline);
-		$('.arrow-left, .arrow-right').css('display', 'none');
-		// Revert back original headline width
-		$('.headline').css('width', '100%')
-	});
+	var baseCamp = $('.basecamp');
+	var highRise = $('.highrise');
+	var campFire = $('.campfire');
+	var mainContent = $('h1, .subline');
+	var hoverContent = $('h2, .subline-replace');
+	var arrow = $('.arrow');
+
+	baseCamp.hover(
+		function(){
+			mainContent.addClass('invisible');
+			hoverContent.removeClass('invisible');
+			arrow.removeClass('invisible');
+		}, function(){
+			hoverContent.addClass('invisible');
+			arrow.addClass('invisible');
+			mainContent.removeClass('invisible');
+		}
+	);
 });
 
+/* Can I make one hover function that selects product specific content specific to what "product" container is being hovered over */
+/* How can I utilize data attribute to select product specific content */
+/* How can I select the arrow image for visibility based on product being hovered over */
+/* Why wont $('h1 .headline') work */
